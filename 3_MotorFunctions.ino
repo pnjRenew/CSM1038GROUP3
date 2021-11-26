6846bd117f871397cc12ddcaa0bfa32eeb1a4ceb
pnjRenew <93779141+pnjRenew@users.noreply.github.com>
2021-11-26 14:02:56 +0000

    Distance adjusted. No drive-in-square. To work with barriers.

--- 3_MotorFunctions.ino
+++ 3_MotorFunctions.ino
@@ -19,6 +19,7 @@
 #define directionLeftLow    9
 #define directionLeftHigh   10
 
+#define avoidDistance 15
 
 //--------------------------------------------------------------------------
 
@@ -70,17 +71,19 @@ void setup() {                               // The setup routine beigns (runnin
 //---------------------------------------------------------------------------
 void loop() {                                // The loop routine beigns (running over and over, continuously).
 
+  //driveInSquare();    // try drive in square
+
   moveForward();                             // Here, we are calling the "moveForward function". This tells the Arduino to
                                              // run a set of code that is written below. 
                                              
   delay(50);                               // "delay" is another example of a function, however this is included
                                              // in the Arduino system, so it knows what to do already.
                                              
-  moveLeft();                                // The brackets in "moveLeft()" are empty because the function does not require
+  //moveLeft();                                // The brackets in "moveLeft()" are empty because the function does not require
                                              // any inputs, whereas "delay(1000)" needs to know the length of the delay
-  delay(0);
-  moveRight();                               // This will run the "moveRight" function, shown below. //
-  delay(0);
+  //delay(0);
+  //moveRight();                               // This will run the "moveRight" function, shown below. //
+  //delay(0);
 
    // Clears the trigPin condition
   digitalWrite(trigPin, LOW);
@@ -98,7 +101,7 @@ void loop() {                                // The loop routine beigns (running
   Serial.print(distance);
   Serial.println(" cm");
 
-  while (distance < 8) {
+  while (distance < avoidDistance) {
     int counter;
     delay(0);   // slow down and stop for 1 second by delaying - or maybe not
     //moveLeft();
@@ -133,11 +136,12 @@ void moveForward(){                          // This is the start of the "moveFo
 
   digitalWrite(directionRightHigh, HIGH);    
   digitalWrite(directionRightLow, LOW);      // This tells the motors to go forward.
-  analogWrite(motorRight, 200);              // This sets the speed.
+  analogWrite(motorRight, 182);              // This sets the speed. - was 200
 
   digitalWrite(directionLeftHigh, HIGH);
   digitalWrite(directionLeftLow, LOW);
-  analogWrite(motorLeft, 200);               // (Q1) Roughly what voltage will this line output, and to what pin number?
+  //analogWrite(motorLeft, 200);               // - was 200 (Q1) Roughly what voltage will this line output, and to what pin number?
+  analogWrite(motorLeft, 200);               // - was 200 (Q1) Roughly what voltage will this line output, and to what pin number?
   
 }
 
@@ -222,14 +226,14 @@ void moveRight(){
 //
 //   returns: none
 //---------------------------------------------------------------------------
-void turnOnSpotLeft(){                          // This is the start of the "moveForward" function.
+void turnOnSpotLeft(){                          // This is the start of the "turnOnSpotLeft" function.
                                              // This function is a "void" as it does not return a value to the loop routine.
 
-  Serial.println("Forward");                 // This will tell you that the "moveForward" funtion is being called
+  Serial.println("Turning on spot Left");                 // This will tell you that the "turnOnSpotLeft" function is being called
                                              // in the serial monitor (Tools > Serial monitor).
 
   digitalWrite(directionRightHigh, HIGH);    
-  digitalWrite(directionRightLow, LOW);      // This tells the motors to go forward.
+  digitalWrite(directionRightLow, LOW);      // This tells the motor to go forward.
   analogWrite(motorRight, 200);              // This sets the speed.
 
   digitalWrite(directionLeftHigh, LOW);
@@ -279,4 +283,31 @@ int findDistance(int tPin, int ePin)
 
   return distance;      // return the distance calculated by Arduino
   
+}
+
+//---------------------------------------------------------------------------
+// Function: driveInSquare
+// ----------------------------
+//   Drive in a square, turning left, to end on start point hopefully 
+//
+
+//
+//   returns: nothing
+//---------------------------------------------------------------------------
+void driveInSquare()
+{
+  moveForward();  // go forward
+  delay(2000);
+  turnOnSpotLeft(); // turn on spot (90)
+  delay(500);   // how much to turn?
+  moveForward(); // go forwarrd again
+  delay(2000);
+  turnOnSpotLeft(); // turn on spot (180)
+  delay(500);   // how much to turn?
+  moveForward(); // go forward 
+  delay(2000);
+  turnOnSpotLeft(); // turn on spot (270)
+  delay(500);   // how much to turn?
+  moveForward(); // go forward
+  delay(2000);
 }
